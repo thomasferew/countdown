a9ac41457376b0161d44f1396e09dc69db45df23
thomasferew <58610700+thomasferew@users.noreply.github.com>
2020-01-31 13:12:24 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+for (let index = 0; index < 4; index++) {
+    game.splash("counting up:" + index)
+}
+    
+
+for (let index = 0; index < 11; index++) {
+    game.splash("counting to 10:" + index)
+}
+
+
+for (let index = 100; index > 0; index--) {
+    game.splash("counting down:" + index)
+}
+
+for (let index4 = 100; index4 < 0; index4-=10) {
+    game.splash("count down by 10" + index4)
+}
